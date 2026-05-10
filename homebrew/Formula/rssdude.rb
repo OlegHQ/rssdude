6a0a3fa330782fdcb83c532e54d3e7883db8135e
Oleg Pustovit <me@opustovit.com>
2026-05-10 16:06:42 +0200

    homebrew: bump formula to v0.3.0

--- homebrew/Formula/rssdude.rb
+++ homebrew/Formula/rssdude.rb
@@ -1,12 +1,12 @@
 class Rssdude < Formula
   desc "Local-first RSS feed reader and content curation CLI"
   homepage "https://github.com/OlegHQ/rssdude"
-  version "0.2.1"
+  version "0.3.0"
 
   on_macos do
     on_arm do
       url "https://github.com/OlegHQ/rssdude/releases/download/v#{version}/rssdude-v#{version}-aarch64-apple-darwin.tar.gz"
-      sha256 "2fa4f5b122accf4a4d569e472e1b2c3505769a33395a753af499ad04f8a2a8ed"
+      sha256 "337501ae6b19c9e023a930c46d8d93a241ede20f11db7fdc35017ad645a34270"
     end
     on_intel do
       odie "rssdude v#{version} ships only as arm64; x86_64 build is planned."
